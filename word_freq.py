2e8a534c7f3e0580d8401df80e2ccd58baf1305e
bwkim0601 <rlaquddn0601@naver.com>
2022-04-12 21:08:30 +0900

    2

--- word_freq.py
+++ word_freq.py
@@ -3,10 +3,11 @@
 import sys
 a = sys.argv[1]
 b = sys.argv[2]
-#print(a)
-f = open(a, 'r')
-line=None
-while line != ' ':
-	line=f.readline()
-	print(line,end='')
+
+f = open("a", 'r')
+
+file = f.read()
+file1 = f.replace("?","")
+
+print(file1)
 f.close()
